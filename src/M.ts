337d368992c3870865b7d86f1b09e13d67b1d5f5
Abourass <Abourassa@AssetVal.com>
2024-05-16 14:00:16 -0600

    :bug: Fixed export field

--- src/M.ts
+++
@@ -1,5 +0,0 @@
-import { All, all } from './All.js';
-import { El, el } from './El.js';
-import { Mote, mote } from './Mote.js';
-
-export { All, all, El, el, Mote, mote };
--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,19 @@ import { resolve, join } from 'path'
 import { defineConfig } from 'vite'
 import dtsPlugin from 'vite-plugin-dts'
 
+const src = resolve(__dirname, 'src')
+
 export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: [
-        join(resolve(__dirname, 'src'), 'M.ts'),
+        join(src, 'All.ts'),
+        join(src, 'Mote.ts'),
+        join(src, 'El.ts'),
       ],
       name: 'Mote',
-      fileName: 'mote',
     },
   },
-  plugins: [dtsPlugin({})],
+  plugins: [dtsPlugin({ bundledPackages: true })],
 })
