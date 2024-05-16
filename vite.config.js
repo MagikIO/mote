0a4e386616cfd233e1e49a73fd1e9130d529d5e4
Abourass <Abourassa@AssetVal.com>
2024-05-16 13:05:14 -0600

    chore: Update package.json version to 1.3.2, improve code organization, and add vite-plugin-dts plugin

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 // vite.config.js
 import { resolve, join } from 'path'
 import { defineConfig } from 'vite'
-
-const root = resolve(__dirname, 'src')
+import dtsPlugin from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: [
-        join(root, 'M.ts'),
+        join(resolve(__dirname, 'src'), 'M.ts'),
       ],
       name: 'Mote',
       fileName: 'mote',
     },
   },
+  plugins: [dtsPlugin({})],
 })
